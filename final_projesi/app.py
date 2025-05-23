240b50327638119870897634b1586632043e8938
Ali Ceylan <ali_cey12@hotmail.com>
2025-05-23 12:17:53 +0300

    Update app.py

--- final_projesi/app.py
+++ final_projesi/app.py
@@ -406,12 +406,5 @@ def download_csv():
         download_name="urunler.csv",
         mimetype="text/csv"
     )
-
-
-#if __name__ == '__main__':
-#   with app.app_context():
-#        db.create_all()
-#    app.run(debug=True)
-
     if __name__ == "__main__":
         app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
